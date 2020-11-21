d486351b2da47be6367ae4cc61c475aa92a4010b
CPASUY <58858439+CPASUY@users.noreply.github.com>
2020-11-20 20:15:17 -0500

    Images

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		FXMLLoader fxmload = new FXMLLoader(getClass().getResource("LoadGame.fxml"));
+		FXMLLoader fxmload = new FXMLLoader(getClass().getResource("BasePane.fxml"));
 		VirtualGameGUIController vgc=new VirtualGameGUIController(primaryStage);
 		fxmload.setController(vgc);
 		Parent root=fxmload.load();
